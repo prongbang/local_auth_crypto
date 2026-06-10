00f79faa9064938973b2a3b0901f98f94458a164
prongbang <prongbang@gmail.com>
2026-06-10 19:32:18 +0700

    migrate: example spm

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -10,17 +10,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:local_auth_crypto_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('renders biometric crypto example', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) =>
-            widget is Text && widget.data!.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
+    expect(find.text('LOCAL AUTH PLUS'), findsOneWidget);
+    expect(find.text('TOKEN'), findsOneWidget);
+    expect(find.text('CIPHER TEXT'), findsOneWidget);
+    expect(find.text('Decrypt by Biometric'), findsOneWidget);
   });
 }
